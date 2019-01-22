90cd6916610a5ec5d8ab88091ad8554be8999ce1
Ray Chen <cccbjcn@163.com>
2019-01-22 17:33:02 +0800

    [Library][NNetwork] - validate message timestamp and timezone

--- Libraries/Nulstar/NNetwork/NMessage.cpp
+++ Libraries/Nulstar/NNetwork/NMessage.cpp
@@ -1,10 +1,13 @@
 #include <QDateTime>
+#include <QTimeZone>
 
 #include "NMessage.h"
 
 namespace NulstarNS {
   qint64 NMessage::mMessageIDPrefix = 0;
   qint64 NMessage::mMessageIDSuffix = 0;
+  const int cTimeZoneNegativeMax = -12;
+  const int cTimeZonePositiveMax = 12;
 
   NMessage::NMessage(const QString& lConnectionName, const QString& lMessageID, QObject *rParent)
           : QObject(rParent), mConnectionName(lConnectionName), mMessageID(lMessageID) {
@@ -39,9 +42,19 @@ namespace NulstarNS {
       qDebug("Message received without 'Timestamp' field!");
       return false;
     }
+    if (!QDateTime::fromSecsSinceEpoch(lMessageObject.value(cTimeStampFieldName).toInt()).isValid()){
+        qDebug("Message timestamp incorrect!");
+        return false;
+    }
     if(!lMessageObject.contains(cTimeZoneFieldName)) {
       qDebug("Message received without 'TimeZone' field!");
       return false;
+    }
+    int lTimeZone = lMessageObject.value(cTimeZoneFieldName).toInt();
+    if ((lTimeZone > cTimeZonePositiveMax) || (lTimeZone < cTimeZoneNegativeMax))
+    {
+          qDebug("Message timezone out of scope!");
+          return false;
     }
     if(!lMessageObject.contains(cMessageTypeFieldName)) {
       qDebug("Message received without 'Type' field!");
