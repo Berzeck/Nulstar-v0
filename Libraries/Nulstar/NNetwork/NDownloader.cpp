7f7eb6538169a6045319e59b3004f31e4f6311f3
Berzeck <berzeck@arxen.com>
2019-12-04 07:57:13 -0400

    [Updater] - Use of lambda C++ functions

--- Libraries/Nulstar/NNetwork/NDownloader.cpp
+++ Libraries/Nulstar/NNetwork/NDownloader.cpp
@@ -1,3 +1,4 @@
+#include <QDir>
 #include <QNetworkRequest>
 #include <QNetworkReply>
 
@@ -8,18 +9,60 @@ namespace NulstarNS {
 
   }
 
-  void NDownloader::fDownload(QUrl lUrl) {
+  void NDownloader::fDownload(const QUrl& lUrl, const QString& lFilePath) {
+  /*  QDir lDir;
+    if(lDir.mkpath(lDownloadPath)) {
+
+    }
+    else {
+      emit sLog(ELogLevel::eLogCritical, ELogMessageType::eResourceManagement, tr("Couldn't create path '%1'. Download aborted.").arg(lDownloadPath));
+      return;
+    }*/
+
     QNetworkRequest lDownloadRequest(lUrl);
     QNetworkReply* rNetworkReply = mNetworkManager.get(lDownloadRequest);
+    QTimer* rDownloadTimer = new QTimer();
+    tDownloadData lDownloadData = {rNetworkReply, QByteArray(), rDownloadTimer, 0};
+    
+    QString lUrlString(lUrl.toString());
+    mDownloadData[lUrlString] = lDownloadData;
 
-   /*   connect(_reply, SIGNAL(downloadProgress(qint64,qint64)), this, SLOT(processDownloadProgress(qint64,qint64)));
-      connect(_reply, SIGNAL(finished()), this, SLOT(processDownloadFinished()));
-      connect(_reply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(onDownloadError(QNetworkReply::NetworkError)));
+    connect(rNetworkReply, &QNetworkReply::downloadProgress, rNetworkReply, [&lUrlString, rNetworkReply, this] (qint64 /*lBytesReceived*/, qint64 /*lBytesTotal*/) {
+        mDownloadData[lUrlString].mFileContents.append(rNetworkReply->readAll()); 
+        mDownloadData[lUrlString].pTimeoutTimer->stop();
+        mDownloadData[lUrlString].pTimeoutTimer->start(cTime_DownloadTimeout * 1000);
+      } );
+    
+    connect(rNetworkReply, &QNetworkReply::finished, rNetworkReply, [&lFilePath, &lUrlString, rNetworkReply, this] () {
+        if(rNetworkReply->error() != QNetworkReply::NoError) {
+          if(mDownloadData[lUrlString].pTimeoutTimer->isActive())
+            mDownloadData[lUrlString].pTimeoutTimer->stop();
+          mDownloadData[lUrlString].pTimeoutTimer->deleteLater();
+          rNetworkReply->deleteLater();
+          mDownloadData.remove(lUrlString);
+        }
+      } );
 
-      connect(reply, &QIODevice::readyRead, this, &MyClass::slotReadyRead);
-      connect(reply, QOverload<QNetworkReply::NetworkError>::of(&QNetworkReply::error),
-              this, &MyClass::slotError);
-      connect(reply, &QNetworkReply::sslErrors,
-              this, &MyClass::slotSslErrors);*/
+    connect(rNetworkReply, QOverload<QNetworkReply::NetworkError>::of(&QNetworkReply::error), rNetworkReply, [&lUrlString, rNetworkReply, this] (QNetworkReply::NetworkError /*lCode*/) {
+        emit sLog(ELogLevel::eLogCritical, ELogMessageType::eResourceManagement, tr("File '%1' couldn't be downloaded. Error: '%2'.").arg(lUrlString).arg(rNetworkReply->errorString()));
+        emit sError(lUrlString);
+      } );
+
+    connect(rDownloadTimer, &QTimer::timeout, rDownloadTimer, [lFilePath, &lUrlString, rDownloadTimer, this] () {
+        if(mDownloadData[lUrlString].mRetryTimes == cCount_RetryTimes) {
+          rDownloadTimer->stop();
+          mDownloadData[lUrlString].pNetworkReply->abort();
+          emit sLog(ELogLevel::eLogCritical, ELogMessageType::eResourceManagement, tr("File '%1' couldn't be downloaded succesfully after '%2' retries.").arg(lUrlString).arg(cCount_RetryTimes));
+          emit sError(lUrlString);
+        }
+        else {
+          rDownloadTimer->stop();
+          mDownloadData[lUrlString].pNetworkReply->abort();
+          emit sLog(ELogLevel::eLogWarning, ELogMessageType::eResourceManagement, tr("File '%1' download timeout, retrying ...").arg(lUrlString));
+          metaObject()->invokeMethod(this, "fDownload", Qt::QueuedConnection, Q_ARG(QUrl, QUrl(lUrlString)), Q_ARG(QString, lFilePath));
+        }
+      } );
+
+    rDownloadTimer->start(cTime_DownloadTimeout * 1000);
   }
 }
--- Libraries/Nulstar/NNetwork/NDownloader.h
+++ Libraries/Nulstar/NNetwork/NDownloader.h
@@ -1,12 +1,15 @@
 #ifndef NDOWNLOADER_H
 #define NDOWNLOADER_H
 
+#include <QByteArray>
 #include <QMap>
 #include <QNetworkAccessManager>
 #include <QObject>
 #include <QString>
 #include <QUrl>
+#include <QTimer>
 
+#include <NLogger.h>
 #include "NNetwork.h"
 
 class QNetworkReply;
@@ -15,17 +18,28 @@ namespace NulstarNS {
   const quint8 cTime_DownloadTimeout = 5;
   const quint8 cCount_RetryTimes = 3; //Retry automatically without emitting an error signal
 
+  struct tDownloadData {
+    QNetworkReply* pNetworkReply;
+    QByteArray mFileContents;
+    QTimer* pTimeoutTimer;
+    quint8 mRetryTimes;
+  };
+
   class NETWORKSHARED_EXPORT NDownloader : public QObject {
     Q_OBJECT
 
     public:
       explicit NDownloader(QObject* rParent = nullptr);
 
-      void fDownload(QUrl, quint8 lRetryTimes = cCount_RetryTimes);
+      Q_INVOKABLE void fDownload(const QUrl &lUrl, const QString &lFilePath);
 
     private:
       QNetworkAccessManager mNetworkManager;
-      QMap<QString, QNetworkReply*> mReplyList; // Url, NetworkReply
+      QMap<QString, tDownloadData> mDownloadData; // Url, DownloadData
+
+    signals:
+      void sLog(ELogLevel eLogLevel, ELogMessageType eLogMessageType, const QString& lMessage);
+      void sError(const QUrl& lUrl);
   };
 }
 
